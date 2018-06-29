fe008b6b281856813cad2b39c0cdef4418d5e4df
Joel-Venzke <joel.venzke@gmail.com>
2018-06-29 16:55:53 -0400

    working on laplacian matrix

--- src/Node_Set.py
+++ src/Node_Set.py
@@ -3,6 +3,7 @@ from sklearn.neighbors import KDTree
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from scipy.special import comb
+from scipy.sparse import csr_matrix
 
 
 class Node_Set:
@@ -14,8 +15,7 @@ class Node_Set:
                  rbf_order=7,
                  poly_order=5,
                  exp_order=3,
-                 node_set_dir="/Users/jvenzke/Repos/RBF_mesh/MD_node_sets"
-                 ):
+                 node_set_dir="/Users/jvenzke/Repos/RBF_mesh/MD_node_sets"):
         self.r_max = r_max
         self.delta_r = delta_r
         self.md_degree = md_degree
@@ -83,32 +83,46 @@ class Node_Set:
         # note it becomes 2D to account for all node locations
         if current_dim == 1:
             # create array with coordinates
-            poly_set = np.tile(nodes[:,-1],(poly_order+1,1)).transpose()
+            poly_set = np.tile(nodes[:, -1], (poly_order + 1, 1)).transpose()
             # deal with constant term
-            poly_set[:,0] = 1.0
+            poly_set[:, 0] = 1.0
             # multiply across rows
             poly_set = np.cumprod(poly_set, axis=1)
             return poly_set
         else:
             # get the array [1, x, x^2, ..., x^(poly_order)] for this dim
-            current_dim_poly = self.get_poly_terms(nodes[:,-1].reshape((nodes.shape[0],1)), poly_order)
+            current_dim_poly = self.get_poly_terms(nodes[:, -1].reshape(
+                (nodes.shape[0], 1)), poly_order)
             # get the array [1, x, y, ..., x^2, xy, y^2, ..., z^2, ..., x^(poly_order)]
-            lower_dim_poly = self.get_poly_terms(nodes[:,:-1], poly_order)
+            lower_dim_poly = self.get_poly_terms(nodes[:, :-1], poly_order)
             # number of poly terms in this dimension
-            num_poly_terms = comb(poly_order+current_dim,current_dim,exact=True)
+            num_poly_terms = comb(
+                poly_order + current_dim, current_dim, exact=True)
             # allocate array
-            poly_set = np.zeros((nodes.shape[0],num_poly_terms))
+            poly_set = np.zeros((nodes.shape[0], num_poly_terms))
             # populate new array
             col = 0
-            for poly_deg in range(0,poly_order+2): # loop over all poly degrees
-                for cur_degree in range(0,poly_deg): # loop over poly degrees
+            for poly_deg in range(
+                    0, poly_order + 2):  # loop over all poly degrees
+                for cur_degree in range(0, poly_deg):  # loop over poly degrees
                     # find upper and lower bound for dim-1 poly list
-                    lower_dim_poly_upper_idx = comb((poly_deg-cur_degree-1)+current_dim-1,current_dim-1,exact=True)
-                    lower_dim_poly_lower_idx = comb((poly_deg-cur_degree-2)+current_dim-1,current_dim-1,exact=True)
+                    lower_dim_poly_upper_idx = comb(
+                        (poly_deg - cur_degree - 1) + current_dim - 1,
+                        current_dim - 1,
+                        exact=True)
+                    lower_dim_poly_lower_idx = comb(
+                        (poly_deg - cur_degree - 2) + current_dim - 1,
+                        current_dim - 1,
+                        exact=True)
                     # find number of poly terms to multiply together
                     col_offset = lower_dim_poly_upper_idx - lower_dim_poly_lower_idx
                     # calculate terms
-                    poly_set[:,col:col+col_offset] = lower_dim_poly[:,lower_dim_poly_lower_idx:lower_dim_poly_upper_idx] * current_dim_poly[:,cur_degree].reshape((current_dim_poly.shape[0],1))
+                    poly_set[:, col:col +
+                             col_offset] = lower_dim_poly[:,
+                                                          lower_dim_poly_lower_idx:
+                                                          lower_dim_poly_upper_idx] * current_dim_poly[:, cur_degree].reshape(
+                                                              (current_dim_poly.
+                                                               shape[0], 1))
                     # update index location
                     col += col_offset
             return poly_set
@@ -130,8 +144,9 @@ class Node_Set:
             self.rotation_matrix_y(-theta), self.rotation_matrix_z(-phi))
 
     def get_average_spacing(self, node_set):
-        nearest_dist, nearest_idx = self.get_nearest_neighbors_from_set(node_set, 2)
-        return nearest_dist[:,1].mean()
+        nearest_dist, nearest_idx = self.get_nearest_neighbors_from_set(
+            node_set, 2)
+        return nearest_dist[:, 1].mean()
 
     def create_node_set(self):
         # read in md node set on shell
@@ -141,18 +156,20 @@ class Node_Set:
                                  str(num_points_per_shell).zfill(5) + ".txt")
 
         average_spacing = self.get_average_spacing(shell_nodes)
-        match_radius = self.delta_r/average_spacing
+        match_radius = self.delta_r / average_spacing
         print "Average spacing on unit sphere:", average_spacing
-        print "Match radius:", match_radius, self.get_average_spacing(shell_nodes*match_radius)
+        print "Match radius:", match_radius, self.get_average_spacing(
+            shell_nodes * match_radius)
         self.node_set = []
         for r in np.arange(self.delta_r / 2.0, self.r_max + self.delta_r / 2.0,
                            self.delta_r):
-            current_shell = shell_nodes * r
-            rotation_matrix = self.rand_rotation_matrix()
-            current_shell[:, :3] = np.transpose(
-                rotation_matrix.dot(np.transpose(current_shell[:, :3])))
-            #TODO: handle weights (account for radius (r integration) and expanding surface area)
-            self.node_set.append(current_shell)
+            if r > match_radius:
+                current_shell = shell_nodes * r
+                rotation_matrix = self.rand_rotation_matrix()
+                current_shell[:, :3] = np.transpose(
+                    rotation_matrix.dot(np.transpose(current_shell[:, :3])))
+                #TODO: handle weights (account for radius (r integration) and expanding surface area)
+                self.node_set.append(current_shell)
         self.node_set = np.concatenate(self.node_set)
         return self.node_set[:, :3], self.node_set[:, 3]
 
@@ -164,55 +181,84 @@ class Node_Set:
         tree = KDTree(node_set)
         return tree.query(node_set, k=k)
 
-    def rbf_phi(self,radius):
+    def rbf_phi(self, radius):
         return radius**self.rbf_order
 
+    def get_row_and_col(self):
+        row_col_set = np.zeros(
+            (self.laplace_weights.shape[0] * self.laplace_weights.shape[1], 2))
+        current_idx = 0
+        for row_idx in self.nearest_idx:
+            row_col_set[current_idx:current_idx + row_idx.shape[0],
+                        0] = row_idx[0]
+            row_col_set[current_idx:current_idx + row_idx.shape[0],
+                        1] = row_idx
+            current_idx += row_idx.shape[0]
+        return row_col_set.transpose()
+
     def calculate_opperator_weights(self):
+        print "Calculating operator weights"
         self.laplace_weights = np.zeros(self.nearest_idx.shape)
 
-        num_poly_terms = comb(self.poly_order+3,3,exact=True)
+        num_poly_terms = comb(self.poly_order + 3, 3, exact=True)
 
         for idx, node_list_idx in enumerate(self.nearest_idx):
+            if idx % int(self.nearest_idx.shape[0] / 10) == 0:
+                print ".",
             # shift nodes to origin
             node_list = self.node_set[node_list_idx]
             node_list_shifted = node_list - node_list[0]
 
-            matrix_size = self.stencil_size+num_poly_terms+self.exp_order
+            matrix_size = self.stencil_size + num_poly_terms + self.exp_order
 
             # create A matrix and right hand side
-            A_matrix = np.zeros((matrix_size,matrix_size))
+            A_matrix = np.zeros((matrix_size, matrix_size))
             right_hand_side = np.zeros((matrix_size))
 
             # fill normal A matrix
             for node_idx, row_node in enumerate(node_list_shifted):
-                A_matrix[node_idx, :self.stencil_size] = self.rbf_phi(np.sqrt(((node_list_shifted-row_node)**2).sum(axis=1)))
-                right_hand_side[node_idx] = self.rbf_order*(self.rbf_order+3.0-2.0)*np.sqrt(((row_node)**2).sum())**(self.rbf_order-2)
-
+                A_matrix[node_idx, :self.stencil_size] = self.rbf_phi(
+                    np.sqrt(((node_list_shifted - row_node)**2).sum(axis=1)))
+                right_hand_side[node_idx] = self.rbf_order * (
+                    self.rbf_order + 3.0 - 2.0) * np.sqrt(
+                        ((row_node)**2).sum())**(self.rbf_order - 2)
 
             # add poly
             for poly_idx in np.arange(num_poly_terms):
-                if poly_idx == 4 or poly_idx == 6 or poly_idx == 9 :
-                    right_hand_side[self.stencil_size+poly_idx] = 2.0
-            A_matrix[:self.stencil_size, self.stencil_size:self.stencil_size+num_poly_terms] = self.get_poly_terms(node_list_shifted, self.poly_order)
-            A_matrix[self.stencil_size:self.stencil_size+num_poly_terms, :self.stencil_size] = A_matrix[:self.stencil_size, self.stencil_size:self.stencil_size+num_poly_terms].transpose()
+                if poly_idx == 4 or poly_idx == 6 or poly_idx == 9:
+                    right_hand_side[self.stencil_size + poly_idx] = 2.0
+            A_matrix[:self.stencil_size, self.stencil_size:
+                     self.stencil_size + num_poly_terms] = self.get_poly_terms(
+                         node_list_shifted, self.poly_order)
+            A_matrix[self.stencil_size:self.stencil_size + num_poly_terms, :
+                     self.stencil_size] = A_matrix[:self.stencil_size,
+                                                   self.stencil_size:
+                                                   self.stencil_size +
+                                                   num_poly_terms].transpose()
 
             # add exp (use unshifted distance)
-            for a in np.arange(1,self.exp_order+1):
+            for a in np.arange(1, self.exp_order + 1):
                 node_list_radius = np.sqrt(((node_list)**2).sum(axis=1))
-                right_hand_side[matrix_size - self.exp_order - 1 + a
-                    ] = a*a*np.exp(-a*node_list_radius[0]
-                        ) - 2.0*a/node_list_radius[0]
-                exp_row = np.exp(-a*node_list_radius)
-                A_matrix[:self.stencil_size,
-                         matrix_size - self.exp_order-1+a] = exp_row
-                A_matrix[matrix_size - self.exp_order - 1 + a,
-                         :self.stencil_size] = exp_row
+                right_hand_side[matrix_size
+                                - self.exp_order - 1 + a] = a * a * np.exp(
+                                    -a * node_list_radius[0]
+                                ) - 2.0 * a / node_list_radius[0]
+                exp_row = np.exp(-a * node_list_radius)
+                A_matrix[:self.stencil_size, matrix_size - self.exp_order - 1 +
+                         a] = exp_row
+                A_matrix[matrix_size - self.exp_order - 1 + a, :
+                         self.stencil_size] = exp_row
 
             # solve and store node weights (drop poly and exp terms)
-            weights = np.linalg.solve(A_matrix,right_hand_side)
-            self.laplace_weights[idx,:] = weights[:self.stencil_size]
-        print self.laplace_weights
-
+            weights = np.linalg.solve(A_matrix, right_hand_side)
+            self.laplace_weights[idx, :] = weights[:self.stencil_size]
+        print
+        self.laplace = csr_matrix(
+            (self.laplace_weights.reshape((self.laplace_weights.shape[0] *
+                                           self.laplace_weights.shape[1], )),
+             self.get_row_and_col()),
+            shape=(self.node_set.shape[0], self.node_set.shape[0]))
+        print self.laplace
 
     def check_quality(self):
         print "Calculating Quality of Node Set"
--- src/mesh.py
+++ src/mesh.py
@@ -1,22 +1,45 @@
 from Node_Set import Node_Set
 import numpy as np
-from scipy.special import comb
+import matplotlib.pyplot as plt
+from scipy.sparse.linalg import eigs
 # notes:
 # random rotation cleans up the issue at large r (I think)
 # the small r limit needs to be fixed. A quasi uniform node layout should do this
 # find average distance to nearest neighbor on unit sphere and match this to delta_r
 # scaling by the radius. use this distance for outer box of quasi uniform node set
 
+node_set = Node_Set(
+    r_max=100,
+    delta_r=0.4,
+    stencil_size=50,
+    md_degree=31,
+    rbf_order=7,
+    poly_order=5,
+    exp_order=1, )
 
-node_set = Node_Set(r_max=10,
-                 delta_r=1.0,
-                 stencil_size=50,
-                 md_degree=6,
-                 rbf_order=7,
-                 poly_order=2,
-                 exp_order=3,)
-
-node_set.check_quality()
+# node_set.check_quality()
 node_set.calculate_opperator_weights()
 
-function = node_set.node_set[:,0]**2 + 3.*node_set.node_set[:,0]**2 + 4.*node_set.node_set[:,0]**2
+function = 1. * node_set.node_set[:,
+                                  0]**2 + 1. * node_set.node_set[:,
+                                                                 1]**2 + 1. * node_set.node_set[:,
+                                                                                                2]**2 + 4
+
+plt.scatter(node_set.laplace.nonzero()[1], -node_set.laplace.nonzero()[0], s=1)
+plt.axis('equal')
+plt.show()
+function = node_set.laplace.dot(function)
+
+plt.semilogy(node_set.node_set[:, 0], function, 'o')
+plt.xlabel("x-axis")
+plt.show()
+plt.semilogy(node_set.node_set[:, 1], function, 'o')
+plt.xlabel("y-axis")
+plt.show()
+plt.semilogy(node_set.node_set[:, 2], function, 'o')
+plt.xlabel("z-axis")
+plt.show()
+
+plt.semilogy(function, 'o')
+plt.xlabel("index")
+plt.show()
